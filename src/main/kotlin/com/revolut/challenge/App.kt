7fb0799e2f05654baaceabf527f5edc8a5358be7
Giovani Chaves <giovanichaves@gmail.com>
2019-07-31 09:52:23 +0200

    OpenApi swagger docs

--- src/main/kotlin/com/revolut/challenge/App.kt
+++ src/main/kotlin/com/revolut/challenge/App.kt
@@ -2,17 +2,34 @@ package com.revolut.challenge
 
 import com.revolut.challenge.transfer.TransferController
 import io.javalin.Javalin
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.ui.SwaggerOptions
+import io.swagger.v3.oas.models.info.Info
+
 
 class App(private val port: Int) {
 
     fun init(): Javalin {
-        val app = Javalin.create { config ->
-            config.defaultContentType = "application/json"
+        val app = Javalin.create {
+            it.showJavalinBanner = false
+            it.defaultContentType = "application/json"
+            it.registerPlugin(OpenApiPlugin(getOpenApiOptions()))
         }.start(port)
 
         TransferController(app)
 
         return app
+    }
+
+    private fun getOpenApiOptions(): OpenApiOptions {
+        val applicationInfo = Info()
+            .version("1.0")
+            .description("Revolut coding challenge")
+
+        return OpenApiOptions(applicationInfo).path("/swagger-docs")
+            .swagger(SwaggerOptions("/swagger").title("My Swagger Documentation"))
+
     }
 }
 
--- src/main/kotlin/com/revolut/challenge/transfer/TransferController.kt
+++ src/main/kotlin/com/revolut/challenge/transfer/TransferController.kt
@@ -2,6 +2,10 @@ package com.revolut.challenge.transfer
 
 import io.javalin.Javalin
 import io.javalin.http.Context
+import io.javalin.plugin.openapi.annotations.OpenApi
+import io.javalin.plugin.openapi.annotations.OpenApiContent
+import io.javalin.plugin.openapi.annotations.OpenApiRequestBody
+import io.javalin.plugin.openapi.annotations.OpenApiResponse
 import javax.servlet.http.HttpServletResponse.SC_OK
 
 
@@ -13,6 +17,10 @@ class TransferController(
         javalinApp.post("/transfer", ::transfer)
     }
 
+    @OpenApi(
+        requestBody = OpenApiRequestBody(content = [OpenApiContent(from = TransferRequest::class)]),
+        responses = [OpenApiResponse(status = "200")]
+    )
     fun transfer(ctx: Context) {
         val transferRequest = ctx.body<TransferRequest>()
         ctx.status(SC_OK)
